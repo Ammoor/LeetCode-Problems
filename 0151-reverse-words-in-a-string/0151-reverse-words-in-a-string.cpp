ddaec791c55da93b7d9edf67da3aed1c10f679ea
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2025-03-02 13:08:54 +0200

    Time: 4 ms (30.07%), Space: 10.2 MB (77.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,12 +3,8 @@ public:
     string reverseWords(string s) {
         string ans = "";
         int cnt = 0;
-        bool flag = false;
         for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] != ' ' && flag == false) {
-                cnt++;
-                flag = true;
-            } else if (s[i] != ' ' && flag == true) {
+            if (s[i] != ' ' && cnt >= 0) {
                 cnt++;
             } else if (s[i] == ' ' && cnt >= 1) {
                 ans += (s.substr(i + 1, cnt) + " ");
