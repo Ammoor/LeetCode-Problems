6f0be688279207e1fe007f21784ec5cf6bb2b3f3
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2025-03-02 12:52:27 +0200

    Time: 0 ms (100%), Space: 10.2 MB (77.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -12,7 +12,7 @@ public:
                 cnt++;
             } else if (s[i] == ' ' && cnt >= 1) {
                 ans += (s.substr(i + 1, cnt) + " ");
-                flag = false;
+                // flag = false;
                 cnt = 0;
             }
         }
