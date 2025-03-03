7a3eb0d391eea7e9314c293eac5c07fe8f5d7fb4
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2025-03-03 11:37:30 +0200

    Time: 12 ms (25.48%), Space: 137.8 MB (29.24%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int cnt = 0;
-        vector<int> small, large;
+        vector<int> small;
+        deque<int> large;
         for (auto it : nums) {
             if (it == pivot) {
-                cnt++;
+                large.push_front(it);
             } else if (it > pivot) {
                 large.push_back(it);
             } else {
                 small.push_back(it);
             }
         }
-        while (cnt--) {
-            small.push_back(pivot);
+        for (auto it : large) {
+            small.push_back(it);
         }
-        small.insert(small.end(), large.begin(),
-                     large.end()); // append large vector to small vector.
         return small;
     }
 };
